3383d4c3066de4bdf9eee38ef2924661d946808c
Rafał Zubala <rzubala@gmail.com>
2024-12-23 11:48:20 +0100

    2025

--- src/main/java/com/zubala/rafal/parser/Parser.java
+++ src/main/java/com/zubala/rafal/parser/Parser.java
@@ -4,36 +4,31 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Parser {
-    private final static int YEAR = 2024;
+    private final static int YEAR = 2025;
     private final static String RESOURCES = "resources";
-    private final static String BIO_FILE = "bio.txt";
-    private final static String ZMIESZANE_FILE = "zmieszane.txt";
+    private final static String BIO_ZMIESZANE_FILE = "zmieszane_bio.txt";
     private final static String CHOINKI_FILE = "choinki.txt";
-    private final static String PLASTIKI_FILE = "metale_plastiki.txt";
+    private final static String PAPIER_PLASTIKI_FILE = "papier_metale_plastiki.txt";
     private final static String GABARYTY_FILE = "gabaryty.txt";
-    private final static String PAPIER_FILE = "papier.txt";
     private final static String SZKŁO_FILE = "szkło.txt";
     private final static String ZIELONE_FILE = "zielone.txt";
-    private final static String CSV_FILENAME = "garbages.csv";
+    private final static String CSV_FILENAME = "garbages_2025.csv";
     private final static String CSV_HEADER = "Subject,Start Date,All Day Event,Description";
     private final static String SUBJECT = "Odbiór śmieci";
 
     enum GarbageType {
-        BIO(1, "biodegradowalne"),
-        MIXED(2, "odpady zmieszane"),
-        TREE(3, "choinki"),
-        PLASTIC(4, "metale i tworzywa sztuczne"),
-        GABARAGE(5, "gabaryty"),
-        GREEN(6, "zielone"),
-        GLASS(7, "opakowania szklane"),
-        PAPER(8,"papier");
+        BIO_MIXED(1, "zmieszane i biodegradowalne"),
+        TREE(2, "choinki"),
+        PAPER_PLASTIC(3, "papier, metale i tworzywa sztuczne"),
+        GARBAGE(4, "gabaryty"),
+        GREEN(5, "zielone"),
+        GLASS(6, "opakowania szklane");
         private final String description;
         private final int id;
 
@@ -88,12 +83,10 @@ public class Parser {
     }
 
     private void readFiles() {
-        readFile(BIO_FILE, GarbageType.BIO);
-        readFile(ZMIESZANE_FILE, GarbageType.MIXED);
-        readFile(PAPIER_FILE, GarbageType.PAPER);
-        readFile(PLASTIKI_FILE, GarbageType.PLASTIC);
+        readFile(BIO_ZMIESZANE_FILE, GarbageType.BIO_MIXED);
+        readFile(PAPIER_PLASTIKI_FILE, GarbageType.PAPER_PLASTIC);
         readFile(SZKŁO_FILE, GarbageType.GLASS);
-        readFile(GABARYTY_FILE, GarbageType.GABARAGE);
+        readFile(GABARYTY_FILE, GarbageType.GARBAGE);
         readFile(CHOINKI_FILE, GarbageType.TREE);
         readFile(ZIELONE_FILE, GarbageType.GREEN);
     }
